7e0a84566ceeca9bbcca13a7f2c0b82ce16fb83e
Greg Merz <gmerz4737@sdsu.edu>
2021-03-16 22:09:41 -0700

    Refactored code

--- pagetable.cpp
+++ pagetable.cpp
@@ -2,40 +2,46 @@
 #include <unistd.h>
 #include <cmath>
 
-struct PageTable{
-	Level *rootNodePtr;
+struct PageTable
+{
+    Level *rootNodePtr;
     int levelCount;
     int *bitMaskAry;
     int *shiftAry;
     int *entryCount;
 
-    PageTable(Level *m_rootNodePtr, int m_levelCount, int *m_bitMaskAry, int *m_shiftAry, int *m_entryCount) {
+    PageTable(Level *m_rootNodePtr, int m_levelCount, int *m_bitMaskAry, int *m_shiftAry, int *m_entryCount)
+    {
         rootNodePtr = m_rootNodePtr;
         levelCount = m_levelCount;
         bitMaskAry = m_bitMaskAry;
         shiftAry = m_shiftAry;
         entryCount = m_entryCount;
     }
-}; 
+};
 
-struct Level {
+struct Level
+{
     PageTable *pageTablePtr;
     int depth;
     void *nextLevelPtr;
 
-    Level(PageTable *m_pageTablePtr, int m_depth, void *m_nextLevelPtr) {
+    Level(PageTable *m_pageTablePtr, int m_depth, void *m_nextLevelPtr)
+    {
         pageTablePtr = m_pageTablePtr;
         depth = m_depth;
         nextLevelPtr = m_nextLevelPtr;
     }
 };
 
-struct Map {
-	int *virtualPage;
+struct Map
+{
+    int *virtualPage;
     int *physicalFrame;
 };
 
-unsigned int logicalToPage(unsigned int logicalAddress, unsigned int mask, unsigned int shift) {
+unsigned int logicalToPage(unsigned int logicalAddress, unsigned int mask, unsigned int shift)
+{
     unsigned int pageTable;
 
     pageTable = logicalAddress & mask;
@@ -44,17 +50,19 @@ unsigned int logicalToPage(unsigned int logicalAddress, unsigned int mask, unsig
     return pageTable;
 }
 
-Map * pageLookup(PageTable *pageTable, unsigned int LogicalAddress) {
+Map *pageLookup(PageTable *pageTable, unsigned int LogicalAddress)
+{
     Map *map;
 
     return map;
 }
 
-void pageInsert(PageTable *pageTable, unsigned int LogicalAddress, unsigned int frame) {
-
+void pageInsert(PageTable *pageTable, unsigned int LogicalAddress, unsigned int frame)
+{
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     int option;
     int idx;
     int numberOfAddresses;
@@ -62,15 +70,17 @@ int main(int argc, char **argv) {
     char *traceFile;
     int hitCounter;
     int missCounter;
-		
+
     // arguments with flags infront
-    while ( (option = getopt(argc, argv, "n:o:")) != -1) {
-        switch (option) {
-        case 'n': 
-        		numberOfAddresses = atoi(optarg);
+    while ((option = getopt(argc, argv, "n:o:")) != -1)
+    {
+        switch (option)
+        {
+        case 'n':
+            numberOfAddresses = atoi(optarg);
             break;
         case 'o':
-        /*
+            /*
         	*mode = optarg;
             if ()
             if ()
@@ -84,26 +94,27 @@ int main(int argc, char **argv) {
         }
     }
 
-	idx = optind;
+    idx = optind;
     traceFile = argv[idx];
- 		
+
     int addressBits = 32;
     int levelCount = argc - (optind + 1);
     PageTable pageTable(NULL, levelCount, new int[levelCount], new int[levelCount], new int[levelCount]);
-    
+
     int level = 0;
-	// all arguments following flagged inputs
-	for (idx = optind + 1; idx < argc; idx++) {
-    	int bits = atoi(argv[idx]);
+    // all arguments following flagged inputs
+    for (idx = optind + 1; idx < argc; idx++)
+    {
+        int bits = atoi(argv[idx]);
         addressBits -= bits;
         pageTable.shiftAry[level] = addressBits;
         pageTable.entryCount[level] = pow(2, bits);
         pageTable.bitMaskAry[level] = (pageTable.entryCount[level] - 1) << pageTable.shiftAry[level];
-        
+
         level++;
     }
-    
+
     //Level
-    Level firstLevel(&pageTable, 0, new Level*[pageTable.entryCount[0] - 1]);
+    Level firstLevel(&pageTable, 0, new Level *[pageTable.entryCount[0] - 1]);
     pageTable.rootNodePtr = &firstLevel;
 }
