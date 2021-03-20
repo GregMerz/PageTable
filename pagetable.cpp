a6ab21cf4cfeba29c76358f7055259bd4e25f191
Greg Merz <gmerz4737@sdsu.edu>
2021-03-20 13:24:03 -0700

    Fix segmentation fault with pointers

--- pagetable.cpp
+++ pagetable.cpp
@@ -3,49 +3,29 @@
 #include <cmath>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+#include "output_mode_helpers.h"
 
 struct PageTable
 {
     void *rootNodePtr;
     int levelCount;
-    int *bitMaskAry;
+    unsigned int *bitMaskAry;
     int *shiftAry;
     int *entryCount;
-
-    PageTable(void *m_rootNodePtr, int m_levelCount, int *m_bitMaskAry, int *m_shiftAry, int *m_entryCount)
-    {
-        rootNodePtr = m_rootNodePtr;
-        levelCount = m_levelCount;
-        bitMaskAry = m_bitMaskAry;
-        shiftAry = m_shiftAry;
-        entryCount = m_entryCount;
-    }
 };
 
 struct Level
 {
     void *pageTablePtr;
     int depth;
-    void **nextLevelPtr;
-
-    Level(void *m_pageTablePtr, int m_depth, void **m_nextLevelPtr)
-    {
-        pageTablePtr = m_pageTablePtr;
-        depth = m_depth;
-        nextLevelPtr = m_nextLevelPtr;
-    }
+    void **nextPtr;
 };
 
 struct Map
 {
     bool valid;
     int frame;
-
-    Map(bool m_valid, int m_frame)
-    {
-        valid = m_valid;
-        frame = m_frame;
-    }
 };
 
 unsigned int logicalToPage(unsigned int logicalAddress, unsigned int mask, unsigned int shift)
@@ -64,35 +44,57 @@ Map *pageLookup(PageTable *pageTable, unsigned int logicalAddress)
     int pageNumber;
 
     Level *currLevel = (Level *)pageTable->rootNodePtr;
+
     for (int depth = 0; depth < leafDepth; depth++)
     {
         pageNumber = logicalToPage(logicalAddress, pageTable->bitMaskAry[depth], pageTable->shiftAry[depth]);
 
-        if (currLevel->nextLevelPtr[pageNumber] == NULL)
+        if (currLevel->nextPtr[pageNumber] == NULL)
         {
             return NULL;
         }
 
-        currLevel = (Level *)currLevel->nextLevelPtr[pageNumber];
+        currLevel = (Level *)currLevel->nextPtr[pageNumber];
     }
 
     pageNumber = logicalToPage(logicalAddress, pageTable->bitMaskAry[leafDepth], pageTable->shiftAry[leafDepth]);
-    Map *map = (Map *)currLevel->nextLevelPtr[pageNumber];
+    Map *map = (Map *)currLevel->nextPtr[pageNumber];
 
     // if the map is valid return it, otherwise return NULL
     return (map->valid) ? map : NULL;
 }
 
-void pageInsert(PageTable *pageTable, unsigned int logicalAddress, unsigned int frame)
+Level **makeLevelList(PageTable *pageTable, int depth)
 {
-    pageInsert((Level *)pageTable->rootNodePtr, logicalAddress, frame);
+    int size = (pageTable->entryCount[depth]) - 1;
+    Level **nextLevelPtr = new Level *[size];
+
+    for (int idx = 0; idx < size; idx++)
+    {
+        nextLevelPtr[idx] == NULL;
+    }
+
+    return nextLevelPtr;
+}
+
+Map **makeMapList(PageTable *pageTable, int depth)
+{
+    int size = (pageTable->entryCount[depth]) - 1;
+    Map **mapPtr = new Map *[size];
+
+    for (int idx = 0; idx < size; idx++)
+    {
+        mapPtr[idx] = new Map(false, -1);
+    }
+
+    return mapPtr;
 }
 
 void pageInsert(Level *currLevel, unsigned int logicalAddress, unsigned int frame)
 {
     PageTable *pageTable = (PageTable *)currLevel->pageTablePtr;
     int depth = currLevel->depth;
-    int bitMask = pageTable->bitMaskAry[depth];
+    unsigned int bitMask = pageTable->bitMaskAry[depth];
     int shift = pageTable->shiftAry[depth];
 
     int pageNumber = logicalToPage(logicalAddress, bitMask, shift);
@@ -101,35 +103,38 @@ void pageInsert(Level *currLevel, unsigned int logicalAddress, unsigned int fram
     int leafDepth = (pageTable->levelCount) - 1;
     if (depth == leafDepth)
     {
-        Map *map = (Map *)currLevel->nextLevelPtr;
-        map = new Map(true, frame);
+        // check if this is making sense later
+        Map **map = (Map **)currLevel->nextPtr;
+        map[pageNumber]->valid = true;
+        map[pageNumber]->frame = frame;
     }
     else
     {
+        void **nextPtr = (depth + 1 == leafDepth) ? (void **)makeMapList(pageTable, depth + 1) : (void **)makeLevelList(pageTable, depth + 1);
+
+        Level *newLevel = new Level(pageTable, depth + 1, nextPtr);
+
+        currLevel->nextPtr[pageNumber] = (void *)newLevel;
+        pageInsert(newLevel, logicalAddress, frame);
     }
+}
+
+void pageInsert(PageTable *pageTable, unsigned int logicalAddress, unsigned int frame)
+{
+    pageInsert((Level *)(pageTable->rootNodePtr), logicalAddress, frame);
 }
 
 int main(int argc, char **argv)
 {
     // declare starting structs
-    PageTable *pageTable;
-    Level *firstLevel;
+    PageTable pageTable;
+    Level firstLevel;
 
-    // if -n flag called, says how many addresses to load
-    int numberOfAddresses = -1;
-
-    // if -o flag called, gets char*
-    char *mode;
-
-    // file where we read the addresses from
-    char *traceFile;
-
-    // when page exists hitCounter increments, otherwise missCounter increments
-    int hitCounter;
-    int missCounter;
-
-    //variable for getopt
+    //variables for getopt
     int option;
+    OutputOptionsType outputType;
+    char *mode;
+    int numberOfAddresses = -1;
 
     // arguments with flags infront
     while ((option = getopt(argc, argv, "n:o:")) != -1)
@@ -138,32 +143,9 @@ int main(int argc, char **argv)
         {
         case 'n':
             numberOfAddresses = atoi(optarg);
-            std::cout << numberOfAddresses;
             break;
         case 'o':
             mode = optarg;
-
-            if (strcmp(mode, "bitmasks") == 0)
-            {
-                std::cout << "bitmasks";
-            }
-            if (strcmp(mode, "logical2physical") == 0)
-            {
-                std::cout << "logical2physical";
-            }
-            if (strcmp(mode, "page2frame") == 0)
-            {
-                std::cout << "page2frame";
-            }
-            if (strcmp(mode, "offset") == 0)
-            {
-                std::cout << "offset";
-            }
-            if (strcmp(mode, "summary") == 0)
-            {
-                std::cout << "summary";
-            }
-
             break;
         default:
             std::cout << "This flag doesn't exist";
@@ -171,27 +153,31 @@ int main(int argc, char **argv)
     }
 
     int idx = optind;
-    traceFile = argv[idx];
+    char *traceFile = argv[idx];
 
+    // setup for pagetable
     int addressBits = 32;
     int levelCount = argc - (optind + 1);
-    pageTable = new PageTable((void *)firstLevel, levelCount, new int[levelCount], new int[levelCount], new int[levelCount]);
+    pageTable.rootNodePtr = &firstLevel;
+    pageTable.levelCount = levelCount;
+    pageTable.bitMaskAry = new unsigned int[levelCount];
+    pageTable.shiftAry = new int[levelCount];
+    pageTable.entryCount = new int[levelCount];
 
     int level = 0;
-    // all arguments following flagged inputs
     for (idx = optind + 1; idx < argc; idx++)
     {
         int bits = atoi(argv[idx]);
         addressBits -= bits;
-        pageTable->shiftAry[level] = addressBits;
-        pageTable->entryCount[level] = pow(2, bits);
-        pageTable->bitMaskAry[level] = (pageTable->entryCount[level] - 1) << pageTable->shiftAry[level];
+        pageTable.shiftAry[level] = addressBits;
+        pageTable.entryCount[level] = pow(2, bits);
+        pageTable.bitMaskAry[level] = (pageTable.entryCount[level] - 1) << pageTable.shiftAry[level];
 
         level++;
     }
 
     //Level
-    int levelSize = pageTable->entryCount[0] - 1;
+    int levelSize = pageTable.entryCount[0] - 1;
     Level **levelList = new Level *[levelSize];
 
     for (int i = 0; i < levelSize; i++)
@@ -199,5 +185,83 @@ int main(int argc, char **argv)
         levelList[i] = NULL;
     }
 
-    firstLevel = new Level((void *)pageTable, 0, (void **)levelList);
+    firstLevel.pageTablePtr = &pageTable;
+    firstLevel.depth = 0;
+    firstLevel.nextPtr = (void **)levelList;
+
+    unsigned int offsetMask = pow(2, addressBits) - 1;
+
+    unsigned int addresses[20] = {
+        0x0041F760,
+        0x0041F780,
+        0x0041F740,
+        0x11F5E2C0,
+        0x05E78900,
+        0x13270900,
+        0x004758A0,
+        0x004A30A0,
+        0x0049E110,
+        0x0049E160,
+        0x0044E4F8,
+        0x0044E500,
+        0x0744E520,
+        0x0044E5A0,
+        0x388A65A0,
+        0x0744EDD0,
+        0x0044E5E0,
+        0x0703FF10,
+        0x0044E620,
+        0x1D496620};
+
+    if (strcmp(mode, "bitmasks") == 0)
+    {
+        report_bitmasks(pageTable.levelCount, pageTable.bitMaskAry);
+    }
+
+    if (strcmp(mode, "logical2physical") == 0)
+    {
+        int frame = 0;
+
+        for (int i = 0; i < 20; i++)
+        {
+            Map *map;
+
+            if ((map = pageLookup(&pageTable, addresses[i])) == NULL)
+            {
+                pageInsert((Level *)pageTable.rootNodePtr, addresses[i], frame);
+                report_logical2physical(addresses[i], frame);
+                frame++;
+            }
+
+            else
+            {
+                report_logical2physical(addresses[i], map->frame);
+            }
+        }
+    }
+    if (strcmp(mode, "page2frame") == 0)
+    {
+        for (int idx = 0; idx < 20; idx++)
+        {
+            unsigned int *page = new unsigned int[pageTable.levelCount];
+
+            for (int i = 0; i < pageTable.levelCount; i++)
+            {
+                page[i] = logicalToPage(addresses[i], pageTable.bitMaskAry[i], pageTable.shiftAry[i]);
+            }
+
+            report_pagemap(addresses[idx], pageTable.levelCount, page, idx);
+        }
+    }
+    if (strcmp(mode, "offset") == 0)
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            report_logical2offset(addresses[i], offsetMask);
+        }
+    }
+    if (strcmp(mode, "summary") == 0)
+    {
+        std::cout << "summary";
+    }
 }
